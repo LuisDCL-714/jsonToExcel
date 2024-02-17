03fc670f5052c988bf4867cf6873e66bbfb18763
Luis Calderon <luisdanie7calderon@gmail.com>
2024-02-17 12:26:42 -0500

    Fix app.js
    Hay que agregar Express.js

--- app.js
+++ app.js
@@ -1,33 +1,12 @@
 import "dotenv/config.js"
-import express, { Router } from "express"
-import multer from "multer"
 
-/*import { parseJsonToExcel } from './parseJsonToExcel.js';*/
+import { parseJsonToExcel } from './parseJsonToExcel.js';
 
-/*async function main(){
+async function main(){
   console.log("Previo al proceso");
   const principalFolder = 'files'; // Nombre de la carpeta principal que contiene carpetas de meses
   await parseJsonToExcel(principalFolder);
   console.log("Fin del proceso");
 }
 
-main();*/
-
-const app = express()
-const routerUpload = Router()
-const port = process.env.PORT
-const storage = multer.memoryStorage()
-const upload = multer({ storage: storage })
-
-/*Upload*/
-  routerUpload.post("/", upload.single('files'),async(req, res) => {
-    const files = req.file
-    console.log({files})
-    res.send('Archivo recibido correctamente.')
-  });
-  app.use('/upload',routerUpload)
-/**/
-
-app.listen(port, () => {
-  console.log(`Server On: http://localhost:${port}`)
-});
+main();
